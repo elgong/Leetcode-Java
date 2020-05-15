f8398fb4bfd49ad2ffab557f0d4cbd752c82911e
elgong <1552460315@qq.com>
2020-05-16 00:23:46 +0800

    a

--- Leetcode-Java/src/Test.java
+++ Leetcode-Java/src/Test.java
@@ -9,9 +9,44 @@ public class Test {
     public static void main(String[] args) {
 
 
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        lengthOfLongestSubstring("bbtablud");
 
-        buffer.put("123".getBytes(Charset.forName("utf-8")));
+    }
+
+
+    public static  int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1){
+            return s.length();
+        }
+
+        HashMap<Character, Integer> slipWindows = new HashMap();
+
+        int max = 1;
+        for (int i = 0; i < s.length(); i++){
+
+            //bbtablud
+            if ( !slipWindows.containsKey(s.charAt(i)) ){
+                slipWindows.put(s.charAt(i), i);
+                if (slipWindows.size() > max){
+                    max = slipWindows.size();
+                }
+
+            } else{
+                // 出现重复值， 那么久删除
+                int index = slipWindows.get(s.charAt(i));
+                slipWindows.put(s.charAt(i), i);
+                for (int j = index - 1; j >= 0; j--){
+
+                    if (!slipWindows.containsKey(s.charAt(j)) ){
+                        break;
+                    }else{
+                        slipWindows.remove(s.charAt(j));
+                    }
+                }
+            }
+        }
+
+        return max;
 
     }
 }
